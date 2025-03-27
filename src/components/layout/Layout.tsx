d24313423ea090c6555caedada88a3ad423b6e6c
Zufarbek <rayadunia2021@gmail.com>
2025-03-27 11:08:08 +0000

    Implement full GreenShop project with all components

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import Navbar from '../common/Navbar';
-import Footer from '../common/Footer';
+import Footer from '../Footer';
 
 interface LayoutProps {
   children: React.ReactNode;
