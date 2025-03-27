ce33625f474158057ba1cb8f28fd6c984a5053d3
Zufarbek <rayadunia2021@gmail.com>
2025-03-27 11:10:09 +0000

    Implement all components based on Figma design

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b465668e3a36a1de"; }
+/******/ 		__webpack_require__.h = function() { return "0724e2866b1848aa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
